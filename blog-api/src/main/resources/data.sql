635f74f26136a0e5e8d5a3e8f1edc99d8e1eeb8d
Mathis Cadio <cadio@et.esiea.fr>
2022-03-13 00:38:22 +0100

    Fix sql table name

--- blog-api/src/main/resources/data.sql
+++ blog-api/src/main/resources/data.sql
@@ -1,7 +1,7 @@
-DROP TABLE IF EXISTS article;
+DROP TABLE IF EXISTS articles;
 DROP TABLE IF EXISTS categories;
  
-CREATE TABLE article (
+CREATE TABLE articles (
   id INT AUTO_INCREMENT  PRIMARY KEY,
   category VARCHAR(250) NOT NULL,
   author VARCHAR(250) NOT NULL,
@@ -9,7 +9,7 @@ CREATE TABLE article (
   content TEXT(10000) NOT NULL
 );
  
-INSERT INTO article (category, author, date, content) VALUES
+INSERT INTO articles (category, author, date, content) VALUES
   ('politiques', 'David', '2022-03-12 12:35:18', 'Présidentielle bientôt les votes'),
   ('sports', 'Jean', '2022-03-11 23:50:10', 'Victoire des Français en rugby'),
   ('cultures', 'Lucas', '2022-03-10 09:10:45', 'Concerts prévus prochainement');
