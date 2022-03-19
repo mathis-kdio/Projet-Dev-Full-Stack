5baeb36adcb3e84f2f370d3566bec494eed88c19
Mathis Cadio <cadio@et.esiea.fr>
2022-03-19 23:53:10 +0100

    fix header

--- blog-front/src/Components/Header.js
+++ blog-front/src/Components/Header.js
@@ -1,8 +1,7 @@
 export default function Header() {
     return (
-        
         <header className="App-header">
-            <p class="photo"><img src="image.png" width="200" height="150"/></p>
+            <p class="photo"><img src="image.png" width="200" height="150" alt="logo"/></p>
             <p className="App-title"><center>Developpé par Mathis Cadio et Benjamin Lasseye</center></p>
         </header>
     )
