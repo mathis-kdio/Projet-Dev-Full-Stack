cc7655b0f1246185b14eeac9a90ebff2c21c5122
Mathis Cadio <cadio@et.esiea.fr>
2022-03-19 23:53:37 +0100

    Categories component

---
+++ blog-front/src/Components/categories/Categories.js
@@ -0,0 +1,25 @@
+import Tabs from "../Tabs";
+
+export default function Categories(props) {
+  const categoriesElements = props.data.map(i =>
+    <div
+      key={i.categoryId}
+      title={i.name}
+    >
+      <h3>{i.name}</h3>
+    </div>
+  )
+  
+  if (categoriesElements.length > 0) {
+    return (
+      <Tabs>
+        {categoriesElements}
+      </Tabs>
+    )
+  }
+  else {
+    return(
+      <h3>Pas de catégories</h3>
+    )
+  }
+}
